1aacdd3bd885498eda092c0b7fb00f7ac118503f
Matthew Moocarme <moocarme@gmail.com>
2016-05-25 07:20:22 -0400

    Add in-file comments

--- wikiGetLinks.py
+++ wikiGetLinks.py
@@ -2,6 +2,8 @@
 """
 Created on Sun May 15 19:40:36 2016
 
+Grabs all the links from a wikipedia page and adds it to at sqlite database
+
 @author: matt-666
 """
 
@@ -9,10 +11,15 @@ import sqlite3
 from lxml import html
 import requests
 
+# Open connection
 conn = sqlite3.connect('wikiLinks.sqlite')
 cur = conn.cursor()
 
 cur.executescript('''
+/*
+Uncomment below to reset tables
+*/
+
 /*
 DROP TABLE IF EXISTS Links;
 DROP TABLE IF EXISTS Category;
@@ -32,6 +39,7 @@ CREATE TABLE Category (
 */
 ''')
 
+# Helper function to grab all links from the url tree and insert into database
 def get_urls(url_tree, parent_id):
     for child in url_tree.iter():
         try:
@@ -42,29 +50,19 @@ def get_urls(url_tree, parent_id):
         except: continue
     return None
 
-#oldurl = 'example old url'
-#cur.execute('''INSERT OR IGNORE INTO Links (link) 
-#        VALUES ( ? )''', ( oldurl, ) )
-#cur.execute('SELECT id FROM Links WHERE link = ? ', (oldurl, ))
-#parenturl_id = cur.fetchone()[0]
-#print(parenturl_id )
-
+# Initialise at the main page since it changes everyday
 init_url = 'https://en.wikipedia.org/wiki/Main_Page'
 
-pages = 20
-init = 344
+pages = 20 # get all links from this number of pages
 
-#print(init_url)
+cur.execute('SELECT COUNT(link) FROM Links ')
+init = int(round(cur.fetchone()[0] / 100.)) # start from database length/100 so we dont repeat
 
 for i in range(pages):
-#cur.execute('''INSERT OR IGNORE INTO Links (link) 
-#        VALUES ( ? )''', ( init_url, ) )
-
+    # grab id from inital link
     cur.execute('SELECT link FROM Links WHERE id = ? ', (init + i, ))
-    try:    
-        url = cur.fetchone()[0]
-    except: 
-        url = None
+    try: url = cur.fetchone()[0]
+    except: url = None
     
     if url:
         cur.execute('SELECT id FROM Links WHERE link = ? ', (init_url, ))
@@ -72,29 +70,10 @@ for i in range(pages):
         try:
             page = requests.get('https://en.wikipedia.org' + url)
             tree = html.fromstring(page.content)
-            tree_body = tree.find_class('mw-content-ltr')
-            tree_body=tree_body[0]
-            t1 = get_urls(tree_body, parenturl_id)
+            # all the links in the main content in the class 'mw-content-ltr'
+            tree_body = tree.find_class('mw-content-ltr') 
+            tree_body=tree_body[0] # in list type for some reason
+            t1 = get_urls(tree_body, parenturl_id) # grab the urls from the tree
         except: continue
 conn.commit()
-#print t1.get('href')
-#newtree = tree
-#categories = []
-#
-#while t1.text != None:
-#    t1 = lookup(newtree, 'Categories')
-#    categories.append(t1.text)    
-#    newurl = 'https://en.wikipedia.org' + t1.get('href')
-##    print(t1.text)
-#    newpage = requests.get(newurl)
-#    newtree = html.fromstring(newpage.content)
-#print(categories)
-#
-#cur.execute('''INSERT OR IGNORE INTO Category (topic) 
-#        VALUES ( ? )''', ( categories[-2], ) )
-#cur.execute('SELECT id FROM Category WHERE topic = ? ', (categories[-2], ))
-#category_id = cur.fetchone()[0]
-#
-#cur.execute('''INSERT OR IGNORE INTO Links (link, parent, topic) 
-#        VALUES ( ?, ?, ? )''', ( url, parenturl_id, category_id) )
-#conn.commit()
+
